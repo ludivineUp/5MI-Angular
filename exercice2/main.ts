f93c71b7c3794ec223d1e4fbb4c5e7e7d953e0a0
Ludivine <lcrepin@ludivinecrepin.fr>
2021-10-11 15:01:09 +0200

    debut correction exo 2

--- exercice2/main.ts
+++ exercice2/main.ts
@@ -64,3 +64,64 @@ const appDiv: HTMLElement = document.getElementById('app');
 let htmlCode : string = `<h1>Exercice TypeScript</h1>`;
 
 htmlCode += '<h2>Somme des âges</h2>';
+let ages : Array<number> = users.map(e => e.age);
+htmlCode += '<p>'+String(ages.reduce((e,a) => a += e))+'</p>';
+ 
+htmlCode += '<h2>Listes des noms</h2>';
+htmlCode += '<p>'+users.map(e => e.name)+'</p>';
+
+htmlCode += '<h2>Ajout de frank en utilisateur</h2>';
+users.push({id: franckMonod.id,
+    name: franckMonod.name,
+    age: franckMonod.age,
+    company: {
+      id: null,
+      name: franckMonod.company,
+      location: null
+    },
+    status: franckMonod.status});
+htmlCode += '<p>'+users.map(e => e.name)+'</p>';
+
+htmlCode += '<h2>Liste des alternants</h2>';
+htmlCode += '<p>'+users.filter(e => e.status == 'alternant').map(e => e.name)+'</p>';
+
+htmlCode += '<h2>Thomas devient cofondateur</h2>';
+let t = undefined;
+for(let i in users){
+  if(users[i].name == 'Thomas'){
+    t = users[i];
+  }
+}
+if(t){
+  t.status = 'cofondateur';
+  htmlCode += '<p>'+ t.status + '</p>';
+}else{
+  htmlCode += '<p>Thomas n existe pas</p>';
+}
+
+htmlCode += '<h2>Tous les cofondateurs</h2>';htmlCode += '<p>'+users.filter(e => e.status == 'cofondateur').map(e => e.name)+'</p>';
+
+htmlCode += "<h2>Nom de l'entreprise de l'user indice 2</h2>";
+let user = undefined;
+for(let i in users){
+  if(i == '2'){
+    user = users[i];
+  }
+}
+htmlCode += '<p>'+user.company.name+'</p>';
+
+htmlCode += "<h2>Localisation de l'user 2</h2>";
+htmlCode += '<p>'+user.company.location+'</p>';
+
+htmlCode += "<h2>Changement PRISMO en Cran-Gevrier</h2>";
+for(let i of users){
+  if(i.company != null && i.company.name != null && i.company.name == 'PRISMO'){
+    htmlCode += '<p>'+i.company.location == null? '': i.company.location+'</p>';
+    i.company.location = 'Cran-Gevrier';
+    htmlCode += '<p>devient ';
+    htmlCode += ''+i.company.location == null? '': i.company.location+'</p>';
+  }
+}
+appDiv.innerHTML = htmlCode;
+
+
